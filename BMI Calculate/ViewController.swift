27fb16b9a77fa872e19a3b9c57d108a377495759
Luiza <khaj-luizkin@yandex.ru>
2023-01-30 14:37:05 +0300

    Initial Commit

---
+++ BMI Calculate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMI Calculate
+//
+//  Created by Луиза Самойленко on 30.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
